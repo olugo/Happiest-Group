beda7ec54e892b24c5eb61c99cd2cf3abe847066
Esther-Yu <yy1997@uw.edu>
2019-11-18 15:42:51 -0800

    Third chart

--- scripts/Happiness_VS_Health.R
+++ scripts/Happiness_VS_Health.R
@@ -1,10 +1,12 @@
-year_2017 <- read.csv(file = "data/world-happiness/2017.csv", stringsAsFactors = FALSE)
-
 library(ggplot2)
 library(dplyr)
 
-graph_2017 <- ggplot(data = year_2017, aes(x = Health..Life.Expectancy., y = Happiness.Score, group = 1)) +
-  geom_line ()+
-  geom_point ()+
-  ggtitle("2017 Happiness Score V.S. Health Life.Expectancy")
-graph_2017
+# A function takes in the year and health data from dataset.
+# Makes the Line chart with a title and an x and y variable.
+# Convert it to a graph.
+get_happiness_vs_health <- function(dataset){
+  dataset <- ggplot(data = year_2017, aes(x = Health..Life.Expectancy., y = Happiness.Score, group = 1)) +
+    geom_line ()+
+    geom_point ()+
+    ggtitle("2017 Happiness Score V.S. Health Life.Expectancy")
+}
